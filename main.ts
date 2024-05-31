9538b1e141d389d0384bd070ce0b9ac8f65c764b
iqra <161558082+IqraRameez12@users.noreply.github.com>
2024-05-31 12:02:20 +0500

    this is my number guessing game project

---
+++ main.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*10)
+const answer=await inquirer.prompt([
+    {
+        name:"userGuessingNumber",
+        type:"number",
+        message:"please guess a number between 1-10"
+    },
+]);
+    if(answer.userGuessingNumber===randomNumber){
+        console.log("you guess a right number")
+    }else{
+
+        console.log("you guess a wrong number")
+    }
